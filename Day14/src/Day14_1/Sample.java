4ce7738a2d32d98315ab2c0b25eb8ba34061dec9
Kray05 <108450083+Kray05@users.noreply.github.com>
2022-07-19 17:16:40 +0900

    fds

---
+++ Day14/src/Day14_1/Sample.java
@@ -0,0 +1,10 @@
+package Day14_1;
+
+public class Sample {
+	public static void main(String[] args) {
+		double base = 5.0;
+		double height = 2.0;
+		double area = (base * height)/2;
+		System.out.println("삼각형의 넓이는 :"+ area);
+	}
+}
