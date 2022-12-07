5c644384667098213f02728e0c8c2db135da0506
Kray <108450083+Kray273@users.noreply.github.com>
2022-12-07 10:55:26 +0900

    Hi

---
+++ Java/src/Day105/Hello.java
@@ -0,0 +1,20 @@
+package Day105;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello 자바");
+        //system.out.print("두번째 줄입니다.");
+        //system.out.println("--두번째 줄 뒤에 붙입니다.");
+        System.out.println("두번째 줄입니다." + "--두번째 줄 뒤에 붙입니다.");
+        // 연산자 출력 "자주"사용
+        // 연산자 기능 2가지
+        // 1. 문자열 + 문자열 또는 문자열 + 다른 타입 ==> 문자열 결합
+        // 2. 정수, 실수, + 정수, 실수 ==> 덧셈
+        // 3. 왼쪽부터 연산
+        System.out.println("세번째 줄입니다." + (100+200));
+        // 4. 우선 순위()
+        
+
+
+    }
+}
